fefc4dd7c0b35dec6b13b8b9af25bf9523b72879
Kinarki <nickfriend81@gmail.com>
2015-10-23 17:07:34 -0700

    average function working proper

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ var student_grade = $('#studentGrade');
 function addClick() {
     addStudent();
     updatesStudentList();
+    updateData();
 
 }
 
@@ -61,16 +62,14 @@ function addStudent() {
     };
     //student-array is adding each student object as an array
     student_array.push(student);
-    console.log(student);
-
+    //clears add form after data is added to table
+    cancelClick();
 }
 
 /**
  * clearAddStudentForm - clears out the form values based on inputIds variable
  */
-
-
-}
+//see cancel function
 /**
  * calculateAverage - loop through the global student array and calculate average grade and return that value
  * @returns {number}
@@ -79,19 +78,21 @@ function calculateAverage() {
     var total = 0;  //saving the total value of the loop
     var i;  //variable for the loop
     for(i=0; i<student_array.length; i++) {
+
         //adding to the total with each loop
-        total += student_array[i].grade;
+        total += parseInt(student_array[i].grade);
     }
     //calculating the average
-    var avg = total/student_array.length;
+    var avg = Math.floor(total/student_array.length);
     //appending to the html element
-    $('.avgGrade').append(avg);
+    $('.avgGrade').text(avg);
 }
 /**
  * updateData - centralized function to update the average and call student list update
  */
-function updataData() {
-
+function updateData() {
+    calculateAverage();
+    updatesStudentList()
 }
 /**
  * updateStudentList - loops through global student array and appends each objects data into the student-list-container > list-body
@@ -105,8 +106,6 @@ function updatesStudentList() {
         var student_name_value = student_array[i].name;
         var student_course_value = student_array[i].course;
         var student_grade_value = student_array[i].grade;
-        console.log(student_name_value, student_course_value, student_grade_value);
-        console.log(student_array);
     }
 
 
